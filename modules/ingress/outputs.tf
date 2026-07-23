2e4fae0e3739d3aa83093e278a970d4aa2463954
bryanpow <ramosbusiness544@gmail.com>
2026-07-23 16:09:13 -0400

    Remove ingress LB hostname output after dropping Kubernetes data source.

--- modules/ingress/outputs.tf
+++ modules/ingress/outputs.tf
@@ -1,7 +1,3 @@
-output "nginx_ingress_service_hostname" {
-  value = try(data.kubernetes_service_v1.nginx_ingress_lb.status[0].load_balancer[0].ingress[0].hostname, "pending")
-}
-
 output "acm_certificate_arn" {
   value = aws_acm_certificate_validation.apex.certificate_arn
 }
