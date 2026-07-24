9a44e5bc9d28581dd51799675d7ebc5e194c2d59
bryanpow <ramosbusiness544@gmail.com>
2026-07-24 16:50:07 -0400

    Fix

--- moved.tf
+++
@@ -1,9 +0,0 @@
-moved {
-  from = kubernetes_role_v1.eks_access_argocd
-  to   = module.eks_access_rbac.kubernetes_role_v1.eks_access_argocd
-}
-
-moved {
-  from = kubernetes_role_binding_v1.eks_access_argocd
-  to   = module.eks_access_rbac.kubernetes_role_binding_v1.eks_access_argocd
-}
